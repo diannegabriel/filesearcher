f59e226f476d293a69a0e27e5b55ebb560e8664c
Dianne <mdianne.gabriel@gmail.com>
2022-09-21 10:52:43 -0400

    Update to find parent

--- search.js
+++ search.js
@@ -1,6 +1,9 @@
 const { readdirSync, readFileSync, lstatSync } = require("fs");
 const fs = require("fs");
 const path = require("path");
+let newArr
+let res
+let parentRes
 
 const getDir = (source) => {
   const results = readdirSync(source);
@@ -10,16 +13,30 @@ const getDir = (source) => {
         readFileSync(path.join(source, result)).includes(argument) &&
         path.extname(result).toLowerCase() === extension
       ) {
-        // console.log("\n" + argument + " is found in:");
         dirSource = source.split("/");
         let file = fs.readFileSync(source + "/" + result, "utf8");
         let arr = file.split(/\r?\n/);
         console.log(
-          "\n\t" + dirSource[dirSource.length - 1] + "/" + result + '\n');
+          "\n\t File: " + dirSource[dirSource.length - 1] + "/" + result + '\n');
         arr.forEach((line, idx) => {
           if (line.includes(argument)) {
-            console.log('\t\t' + (idx+1) + ":" + line);
+            newArr = arr.slice(0, idx+1).reverse();
+            res = newArr.find(el => {
+                return el.includes("<name>")
+            })
+            parentRes = res.replace("<name>", "").replace("</name>","").trim()
           }
+        });
+        arr.forEach((line, idx) => {
+          if (line.includes(parentRes)) {
+            if (line.includes("<name>") || line.includes("<blockRef")) {
+              newArr = arr.slice(0, idx+1).reverse();
+                res = newArr.find(el => {
+                    return el.includes("<key>")
+                })
+              console.log(arr.indexOf(res, 0)+1, res)
+            }
+          } 
         });
       }
     } else if (lstatSync(path.join(source, result)).isDirectory()) {
@@ -31,4 +48,5 @@ const getDir = (source) => {
 const dir = process.cwd();
 const extension = ".xml"; //You can change the extension type here
 let argument = process.argv[2];
+console.log("\n" + argument + " is found in:")
 getDir(dir);
